f12deedb627bd999bd35240f803f078a2883322e
tybens <tb19@princeton.edu>
2020-11-16 19:57:50 -0800

    add twitch train/raid button and functionality

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ import datetime
 import redis
 
 from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
 app = Flask(__name__, static_url_path='', static_folder='static')
+CORS(app)
 main_redis = redis.Redis(decode_responses=True, db=0)
 stats_redis = redis.Redis(decode_responses=True, db=1)
 
 
-def getStreams(count = 1):
+def getStreams(count = 1, trainId=None):
     results = []
     for i in range(int(count)):
-        key = main_redis.randomkey()
+        if trainId is None:
+            key = main_redis.randomkey()
+        else:
+            key = main_redis.keys(pattern='*{}*'.format(trainId))[0]
 
         if not key:
             return results
@@ -37,6 +42,23 @@ def get_stream():
     if streams:
         return streams[0]
     return '{}'
+
+@app.route('/streamtrainId/<trainId>')
+def get_stream_train(trainId):
+    print("Trainid: ", trainId)
+    streams = getStreams(trainId=trainId)
+    if streams:
+        return streams[0]
+    return '{}'
+
+@app.route('/streamtrain')
+def get_stream_trainId():
+    key = main_redis.randomkey()
+    stream = json.loads(key)
+    trainId = stream['id']
+
+    return trainId
+
 
 @app.route('/streams', defaults={'count': 20})
 @app.route('/streams/<count>')
