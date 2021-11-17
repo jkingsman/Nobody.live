0744e93015a247da444fb39fd7ecd45a5a7d5d7b
Jack Kingsman <jack.kingsman@gmail.com>
2021-11-16 16:41:45 -0800

    Sanic (#15)
    * Initial sanic spike
    * Complete sanic conversion

--- db_utils.py
+++ db_utils.py
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS metadata (
 conn = psycopg2.connect(f"dbname='{os.environ.get('NOBODY_DATABASE')}' user='{os.environ.get('NOBODY_USER')}' host='{os.environ.get('NOBODY_HOST')}' password='{os.environ.get('NOBODY_PASSWORD')}'")
 conn.autocommit = True
 
+
 def migrate():
     with conn.cursor() as cursor:
         print("Migrating schema")
@@ -41,6 +42,7 @@ def migrate():
         print("Dropping and reloading metadata row")
         cursor.execute("TRUNCATE metadata;")
         cursor.execute("INSERT INTO metadata VALUES (extract(epoch from now() at time zone 'utc'), extract(epoch from now() at time zone 'utc'), 0, 0);")
+
 
 def bulk_insert_streams(streams):
     formatted_rows = [(stream['id'], stream['game_name'], stream['language'], json.dumps(stream)) for stream in streams]
@@ -54,73 +56,6 @@ def bulk_insert_streams(streams):
             cursor, insert_query, formatted_rows, template=None, page_size=100
         )
 
-def set_populate_started():
-    with conn.cursor() as cursor:
-        cursor.execute("UPDATE metadata SET populate_started = extract(epoch from now() at time zone 'utc')")
-
-def set_ratelimit_data(ratelimit_limit, ratelimit_remaining):
-    set_ratelimit_query = """
-        UPDATE metadata SET
-        time_of_ratelimit = extract(epoch from now() at time zone 'utc'),
-        ratelimit_limit = %s,
-        ratelimit_remaining = %s;"""
-
-    with conn.cursor() as cursor:
-        cursor.execute(set_ratelimit_query, (ratelimit_limit, ratelimit_remaining))
-
-def get_games(count, include_list, exclude_list):
-    if not include_list and not exclude_list:
-        # if we have no criteria we can optimize
-        games_query = f"""
-            SELECT data FROM streams
-            TABLESAMPLE system_rows(%s)"""
-
-        with conn.cursor() as cursor:
-            cursor.execute(games_query, [count])
-            return cursor.fetchall()
-    else:
-        games_query = f"""
-            SELECT data FROM streams
-            WHERE 1=1
-            {'AND lower(game) NOT LIKE %s ' * len(exclude_list)}
-            {'AND lower(game) LIKE %s ' * len(include_list)}
-            ORDER BY RANDOM()
-            LIMIT %s"""
-
-    wildcarded_exclusions = [f"%{exclude.lower()}%" for exclude in exclude_list]
-    wildcarded_inclusions = [f"%{include.lower()}%" for include in include_list]
-
-    with conn.cursor() as cursor:
-        cursor.execute(games_query, [*wildcarded_exclusions, *wildcarded_inclusions, count])
-        return cursor.fetchall()
-
-def get_games_list_by_game():
-    games_list_query = """
-        SELECT game, count(*) FROM streams
-        GROUP BY game"""
-
-    with conn.cursor() as cursor:
-        cursor.execute(games_list_query)
-        return cursor.fetchall()
-
-def get_stats():
-    with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
-        cursor.execute("SELECT * FROM metadata;")
-        stats_dict = dict(cursor.fetchone())
-
-        cursor.execute("SELECT count(*) as total FROM streams;")
-        stats_dict['streams'] = cursor.fetchone()['total']
-
-        return stats_dict
-
-def get_stream_details(id):
-    games_query = f"""
-        SELECT * FROM streams
-        WHERE id = %s"""
-
-    with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
-        cursor.execute(games_query, [id])
-        return cursor.fetchone()
 
 def prune(max_age_secs):
     age = time.time() - max_age_secs
