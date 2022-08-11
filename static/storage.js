c34d0824867a8af03f033d2ef4542d011b5ba388
jkingsman <jack.kingsman@gmail.com>
2022-08-11 06:19:12 -0700

    main Rework storage again to be much more storage-based

--- static/storage.js
+++ static/storage.js
@@ -1,147 +1,181 @@
-const SETTINGS = {
-  shouldRemember: {
-    respectsRememberSetting: false,
-    default: false,
-    allowedValues: [true, false],
-  },
-  include: {
-    respectsRememberSetting: true,
-    default: '',
-  },
-  exclude: {
-    respectsRememberSetting: true,
-    default: '',
-  },
-  minDuration: {
-    respectsRememberSetting: true,
-    default: 0,
-    allowedValues: [0, 3, 5, 10],
-  },
-  maxViewers: {
-    respectsRememberSetting: true,
-    default: 0,
-    allowedValues: [0, 1],
-  },
-  searchOperator: {
-    respectsRememberSetting: true,
-    default: 'all',
-    allowedValues: ['all', 'any'],
-  },
-  widePage: {
-    respectsRememberSetting: false,
-    default: false,
-    allowedValues: [true, false],
-  },
-  minimalPage: {
-    respectsRememberSetting: false,
-    default: false,
-    allowedValues: [true, false],
-  },
-  streamHistory: {
-    respectsRememberSetting: false,
-    default: '[]',
-  },
-};
+/* eslint-disable no-console */
+class Settings {
+  constructor() {
+    this.KNOWN_SETTINGS = {
+      shouldRemember: {
+        respectsRememberSetting: false,
+        default: false,
+        allowedValues: [true, false],
+      },
+      include: {
+        respectsRememberSetting: true,
+        default: '',
+      },
+      exclude: {
+        respectsRememberSetting: true,
+        default: '',
+      },
+      minDuration: {
+        respectsRememberSetting: true,
+        default: 0,
+        allowedValues: [0, 3, 5, 10],
+      },
+      maxViewers: {
+        respectsRememberSetting: true,
+        default: 0,
+        allowedValues: [0, 1],
+      },
+      searchOperator: {
+        respectsRememberSetting: true,
+        default: 'all',
+        allowedValues: ['all', 'any'],
+      },
+      widePage: {
+        respectsRememberSetting: false,
+        default: false,
+        allowedValues: [true, false],
+      },
+      minimalPage: {
+        respectsRememberSetting: false,
+        default: false,
+        allowedValues: [true, false],
+      },
+      streamHistory: {
+        respectsRememberSetting: false,
+        default: '[]',
+      },
+    };
 
-function getStoredSetting(settingKey, parse = false) {
-  // check that we know about the setting
-  if (!SETTINGS[settingKey]) {
-    console.error(`Unknown key! ${settingKey} is not one of ${Object.keys(SETTINGS).join(', ')}`);
-    return null;
+    this.ephemeral = {
+      numberOfThumbnailsToFetch: 16,
+      server: document.location.origin,
+      currentStream: null,
+    };
+
+    localStorage.setItem('storageVersion', '0.0.1');
   }
 
-  const requestedSetting = SETTINGS[settingKey];
-
-  // check if we have a stored value
-  if (localStorage.getItem(settingKey) === null) {
-    // key doesn't exist
-    localStorage.setItem(settingKey, requestedSetting.default);
+  static shouldRemember() {
+    return JSON.parse(localStorage.getItem('shouldRemember')) || false;
   }
 
-  let rawSetting;
-  if (requestedSetting.respectsRememberSetting) {
-    if (JSON.parse(localStorage.getItem('shouldRemember')) || false) {
-      // this respects the remember setting, and we should use the remembered value
-      rawSetting = localStorage.getItem(settingKey);
-    } else {
-      // this respects the remember setting, but we are not set to remember right now
-      // give the default
-      return requestedSetting.default;
+  setBulk(settingsObj) {
+    for (const [key, value] of Object.entries(settingsObj)) {
+      this.set(key, value);
     }
-  } else {
-    // doesn't respect remember setting; provide what's in storage
-    rawSetting = localStorage.getItem(settingKey);
   }
 
-  if (parse) {
-    try {
-      return JSON.parse(rawSetting);
-    } catch (error) {
-      console.error(`Parse failure! Could not parse key '${settingKey}' with value '${rawSetting}'.`);
-      return rawSetting;
-    }
-  } else {
-    return rawSetting;
-  }
-}
-
-function setStoredSetting(settingsObj) {
-  localStorage.setItem('storageVersion', '0.0.1');
-  for (const [settingKey, value] of Object.entries(settingsObj)) {
-    // check that we know about the setting
-    if (!SETTINGS[settingKey]) {
-      console.error(`Unknown key! ${settingKey} is not one of ${Object.keys(SETTINGS).join(', ')}`);
+  set(key, value) {
+    // if we haven't heard of it, set it ephemerally
+    if (!this.KNOWN_SETTINGS[key]) {
+      console.log(`7 Setting ${key} to ephemeral (${value})`);
+      this.ephemeral[key] = value;
       return;
     }
 
-    const requestedSetting = SETTINGS[settingKey];
+    const requestedSetting = this.KNOWN_SETTINGS[key];
     if (requestedSetting.allowedValues && !requestedSetting.allowedValues.includes(value)) {
-      console.error(`Illegal value! '${value}' is not one of ${requestedSetting.allowedValues.join(', ')}`);
+      console.error(`Illegal value! Key '${key}' has value '${value}' is not one of \
+${requestedSetting.allowedValues.join(', ')}`);
       return;
     }
 
-    const shouldRemember = JSON.parse(localStorage.getItem('shouldRemember')) || false;
     if (requestedSetting.respectsRememberSetting) {
-      if (shouldRemember) {
+      if (this.constructor.shouldRemember()) {
         // this respects the remember setting, and we should set the remembered value
-        localStorage.setItem(settingKey, value);
+      console.log(`7 Setting ${key} to localstorage (${value})`);
+        localStorage.setItem(key, value);
       } else {
-        // this respects the remember setting, but we are not set to remember right now so store default
-        localStorage.setItem(settingKey, requestedSetting.default);
+        // we are not remembering right now so store default long term and set it in ephemeral
+      console.log(`8 Setting ${key} to default in local storage and ephemeral to (${value})`);
+        localStorage.setItem(key, requestedSetting.default);
+        this.ephemeral[key] = value;
       }
     } else {
       // doesn't respect remember setting; store what we're given
-      localStorage.setItem(settingKey, value);
+      localStorage.setItem(key, value);
+      console.log(`9 Setting ${key} to localstorage (${value})`);
     }
   }
-}
 
-// migrate from older storage engine versions
-function migrateData() {
-  const shouldMigrate = localStorage.getItem('streamHistoryJSON') && !localStorage.getItem('hasMigrated');
-  if (shouldMigrate) {
-    const settingsObj = {};
-
-    try {
-      settingsObj.widePage = JSON.parse(localStorage.getItem('widePage')) || false;
-      settingsObj.minimalPage = JSON.parse(localStorage.getItem('minimalPage')) || false;
-      settingsObj.streamHistory = JSON.stringify(JSON.parse(localStorage.getItem('streamHistoryJSON')) || []);
-
-      settingsObj.shouldRemember = JSON.parse(localStorage.getItem('streamFilterRemember')) || false;
-      if (settingsObj.shouldRemember) {
-        settingsObj.include = JSON.parse(localStorage.getItem('streamFilter')).include;
-        settingsObj.exclude = JSON.parse(localStorage.getItem('streamFilter')).exclude;
-        settingsObj.minDuration = JSON.parse(localStorage.getItem('streamFilter')).minAge;
-        settingsObj.maxViewers = JSON.parse(localStorage.getItem('streamFilter')).max_viewers;
-        settingsObj.searchOperator = JSON.parse(localStorage.getItem('streamFilter')).search_operator;
+  get(key, parse = false) {
+    if (!this.KNOWN_SETTINGS[key]) {
+      // if we haven't heard of it, get it ephemerally
+      if (!this.ephemeral[key]) {
+        console.error(`Unknown key! ${key} is not one of ${Object.keys(this.KNOWN_SETTINGS).join(', ')} nor found in ephemera.`);
+        return null;
       }
-    } catch (error) {
-      console.error("Migration has failed!")
-      console.error(error);
+
+      console.log(`0 Returning ${key} from ephemeral (${this.ephemeral[key]})`);
+      return this.ephemeral[key];
     }
 
-    localStorage.clear();
-    setStoredSetting(settingsObj);
-    localStorage.setItem('hasMigrated', true);
+    const requestedSetting = this.KNOWN_SETTINGS[key];
+    let rawSetting;
+    if (requestedSetting.respectsRememberSetting) {
+      if (this.constructor.shouldRemember()) {
+        // this setting respects remember and remember is set; return from storage
+        rawSetting = localStorage.getItem(key);
+        console.log(`1 Returning ${key} from localstorage (${rawSetting})`);
+      } else {
+        // this setting respects remember and but remember is not set; try for ephemeral or default
+        rawSetting = this.ephemeral[key];
+        if (!rawSetting) {
+          // we don't have this stored; return default
+          rawSetting = requestedSetting.default;
+          console.log(`3 Returning ${key} from default (${rawSetting})`);
+        } else {
+          console.log(`2 Returning ${key} from ephemeral (${rawSetting})`);
+        }
+      }
+    } else {
+      // does not respect remembering; return from localstorage
+      rawSetting = localStorage.getItem(key);
+      if (!rawSetting) {
+        // we don't have this stored; return default
+        rawSetting = requestedSetting.default;
+      }
+    }
+
+    if (parse) {
+      try {
+        return JSON.parse(rawSetting);
+      } catch (error) {
+        console.error(`Parse failure! Could not parse key '${key}' with value '${rawSetting}'.`);
+        return rawSetting;
+      }
+    } else {
+      return rawSetting;
+    }
+  }
+
+  // migrate from legacy storage versions
+  migrateData() {
+    const shouldMigrate = localStorage.getItem('streamHistoryJSON') && !localStorage.getItem('hasMigrated');
+    if (shouldMigrate) {
+      const settingsObj = {};
+
+      try {
+        settingsObj.widePage = JSON.parse(localStorage.getItem('widePage')) || false;
+        settingsObj.minimalPage = JSON.parse(localStorage.getItem('minimalPage')) || false;
+        settingsObj.streamHistory = JSON.stringify(JSON.parse(localStorage.getItem('streamHistoryJSON')) || []);
+
+        settingsObj.shouldRemember = JSON.parse(localStorage.getItem('streamFilterRemember')) || false;
+        if (settingsObj.shouldRemember) {
+          settingsObj.include = JSON.parse(localStorage.getItem('streamFilter')).include;
+          settingsObj.exclude = JSON.parse(localStorage.getItem('streamFilter')).exclude;
+          settingsObj.minDuration = JSON.parse(localStorage.getItem('streamFilter')).minAge;
+          settingsObj.maxViewers = JSON.parse(localStorage.getItem('streamFilter')).max_viewers;
+          settingsObj.searchOperator = JSON.parse(localStorage.getItem('streamFilter')).search_operator;
+        }
+      } catch (error) {
+        console.error('Migration has failed!');
+        console.error(error);
+      }
+
+      localStorage.clear();
+      this.setBulk(settingsObj);
+      localStorage.setItem('hasMigrated', true);
+    }
   }
 }
